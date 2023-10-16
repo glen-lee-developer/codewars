87354e1ac8011828961595bf7770f7e4b0b3d559
glen-lee-developer <glenleecareer@gmail.com>
2023-10-16 04:29:27 +0100

    add Moving Zeros to the End(5)

---
+++ 5 Kyu/Moving_Zeros_to_the_End.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+//  MY SOLUTION
+function moveZeros(arr) {
+  return [...arr.filter((num) => num !== 0), ...arr.filter((num) => num === 0)];
+}
+
+//  FOR FUN - If ANY number contains a 0 then make move that zero to back as well.
+
+function moveZeros(arr) {
+  let nonZeros = [];
+  let zeros = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i]);
+    } else if (arr[i].toString().includes("0")) {
+      nonZeros.push(arr[i]);
+    } else {
+      nonZeros.push(arr[i]);
+    }
+  }
+  return nonZeros.concat(zeros).map((char) => parseInt(char));
+}
