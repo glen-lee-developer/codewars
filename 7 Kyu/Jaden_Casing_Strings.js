9b5ed98feecf051d6e60e0d7f1a5c73cd0ed63b0
glen-lee-developer <glenleecareer@gmail.com>
2023-10-20 10:36:34 +0100

    add Number of people in the bus (7)

--- 7 Kyu/Jaden_Casing_Strings.js
+++ 7 Kyu/Jaden_Casing_Strings.js
@@ -10,8 +10,6 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 Link to Jaden's former Twitter account @officialjaden via archive.org
 
-
-
 */
 
 //  MY SOLUTION
